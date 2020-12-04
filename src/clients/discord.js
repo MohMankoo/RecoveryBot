0b60dcfc745704532c844c90d093845e408e3be8
Mohpreet <mohpreetmankoo@gmail.com>
2020-12-04 15:02:30 -0500

    Add disboard bumping functionality

--- src/clients/discord.js
+++ src/clients/discord.js
@@ -1,37 +1,48 @@
 const { Client } = require('discord.js')
 const { handleMessage } = require('../messages')
 const { setupDB, createUsers } = require('../db')
-const { BOT_TOKEN, GENERAL_CHANNEL_ID, GREETING_MSG } = require('../config')
+const config = require('../config')
 
 // Create a Discord client for our bot
 const bot = new Client()
 
 const startBot = async () => {
   configureBot()
-  await bot.login(BOT_TOKEN)
+  await bot.login(config.BOT_TOKEN)
 }
 
 // Configure the bot to respond to messages
 const configureBot = () => {
   bot.on('ready', () => {
+    // Do initialization routine
     console.log(`DISCORD: ${bot.user.tag || 'Bot'} is now connected`)
-
     bot.user.setActivity('!help')
     setupDB(bot)
+
+    // Schedule disboard if needed; bumps every 121 minutes
+    if (config.USE_DISBOARD_BUMP && config.DISBOARD_CHANNEL_ID) {
+      setInterval(bumpDisboard, 7260000)
+    }
   })
 
   bot.on('message', async message => await handleMessage(message))
 
   bot.on('guildMemberAdd', member => {
+    if (!config.GENERAL_CHANNEL_ID) return
     const memberTag = member.user.tag
     console.log(`DISCORD: New member added: ${memberTag}`)
 
     createUsers([{ name: memberTag }])
-    member.guild.channels.cache
-      .find(channel => channel.id === GENERAL_CHANNEL_ID)
-      .send(`Welcome ${member}! ${GREETING_MSG}`)
+    bot.channels.cache
+      .find(channel => channel.id === config.GENERAL_CHANNEL_ID)
+      .send(`Welcome ${member}! ${config.GREETING_MSG}`)
   })
 }
+
+const bumpDisboard = () =>
+  bot.channels.cache
+    .find(channel => channel.id === config.DISBOARD_CHANNEL_ID)
+    .send(`!d bump`)
 
 // Return the bot instance as well as a means of starting it
 module.exports = { startBot, bot }
--- src/config.js
+++ src/config.js
@@ -14,13 +14,20 @@ const BOT_TOKEN = process.env.BOT_TOKEN || ''
 
 // Other
 // GENERAL_CHANNEL is used for server greetings
-const GENERAL_CHANNEL_ID = process.env.GENERAL_CHANNEL_ID || '0'
+const GENERAL_CHANNEL_ID = process.env.GENERAL_CHANNEL_ID || ''
 const GREETING_MSG = process.env.GREETING_MSG || 'Welcome!'
+
+// DISBOARD_CHANNEL to use for bumping server on Disboard.org
+const USE_DISBOARD_BUMP = process.env.USE_DISBOARD_BUMP || false
+const DISBOARD_CHANNEL_ID =
+  process.env.DISBOARD_CHANNEL_ID || GENERAL_CHANNEL_ID
 
 module.exports = {
   PORT,
   DB_URL,
   BOT_TOKEN,
   GENERAL_CHANNEL_ID,
-  GREETING_MSG
+  GREETING_MSG,
+  USE_DISBOARD_BUMP,
+  DISBOARD_CHANNEL_ID
 }
